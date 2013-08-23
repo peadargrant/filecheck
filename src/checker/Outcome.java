8f5354fad4b3921633d5cbeeb819da37823ac2e5
Peadar Grant <peadargrant@gmail.com>
2013-08-23 17:36:20 +0100

    Colours are now less garish.

--- src/checker/Outcome.java
+++ src/checker/Outcome.java
@@ -20,27 +20,34 @@ public enum Outcome {
     
     private Color color;
     private Color saturatedColor;
-
-    private Outcome(Color color) {
-        
-        this.saturatedColor = color;
-        
+    
+    private Color clampedColour(Color color, int min, int max)
+    {
         ArrayList<Integer> rgb = new ArrayList<>();
         
-        int clamp = 150;
         rgb.add( color.getRed() );
         rgb.add( color.getGreen() );
         rgb.add( color.getBlue() );
         
         for ( int k = 0 ; k < rgb.size() ; k++ )
         {
-            if ( rgb.get(k) < clamp )
+            if ( rgb.get(k) < min )
             {
-                rgb.set(k, clamp);
+                rgb.set(k, min);
+            }
+            else if ( rgb.get(k) > max )
+            {
+                rgb.set(k, max);
             }
         }
+        
+        return new Color(rgb.get(0), rgb.get(1), rgb.get(2)); 
+    }
 
-        this.color = new Color(rgb.get(0), rgb.get(1), rgb.get(2));
+    private Outcome(Color color) {
+        
+        this.saturatedColor = this.clampedColour(color, 0, 100);
+        this.color = this.clampedColour(color, 150, 3000);
     }
     
     public Color getColor()
