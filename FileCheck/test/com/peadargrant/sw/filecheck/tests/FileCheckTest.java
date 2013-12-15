db291e4167f4d5bca26ba86c309530f09728cdc2
Peadar Grant <peadargrant@gmail.com>
2013-12-15 22:38:02 +0000

    stub test added

---
+++ FileCheck/test/com/peadargrant/sw/filecheck/tests/FileCheckTest.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright Peadar Grant.
+ *  All rights reserved.
+ */
+
+package com.peadargrant.sw.filecheck.tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Peadar Grant
+ */
+public class FileCheckTest {
+    
+    public FileCheckTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void fileCheckTestStub()
+    {
+        assertTrue(true);  
+    }
+}
