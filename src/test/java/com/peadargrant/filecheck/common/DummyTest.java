66321e6fa4af5cb8d9997e9e78ff136d2eec67bc
Peadar Grant <peadargrant@gmail.com>
2014-07-11 15:01:37 +0100

    testing infrastructure added

---
+++ src/test/java/com/peadargrant/filecheck/common/DummyTest.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright Peadar Grant.
+ *  All rights reserved.
+ */
+
+package com.peadargrant.filecheck.common;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Peadar Grant
+ */
+public class DummyTest {
+    
+    public DummyTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setAssignmentName method, of class TerminalReport.
+     */
+    @Test
+    public void testSetAssignmentName() {
+        assertTrue("dummy test passed",true);
+    }
+    
+}
