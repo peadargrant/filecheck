cdad1277eb838d3b2ca2d8ea2f78decbdceb3638
Peadar Grant <peadargrant@gmail.com>
2015-02-04 10:17:24 +0000

    can specify rules on a pattern match

--- filecheck-core/src/main/java/com/peadargrant/filecheck/core/checker/Checker.java
+++ filecheck-core/src/main/java/com/peadargrant/filecheck/core/checker/Checker.java
@@ -19,7 +19,10 @@ package com.peadargrant.filecheck.core.checker;
 import com.peadargrant.filecheck.core.assignments.Assignment;
 import com.peadargrant.filecheck.core.assignments.Check;
 import com.peadargrant.filecheck.core.assignments.Content;
+import com.peadargrant.filecheck.core.assignments.Pattern;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -29,9 +32,9 @@ import java.util.jar.JarFile;
  * @author Peadar Grant <peadargrant@gmail.com>
  */
 public class Checker {
-    
+
     private CheckReport report;
-    private HashMap<Check,CheckImplementation> checkImplementations;
+    private HashMap<Check, CheckImplementation> checkImplementations;
 
     /**
      * Get the value of report
@@ -51,180 +54,179 @@ public class Checker {
         this.report = report;
     }
 
-    
-    public Checker()
-    {
+    public Checker() {
         this.report = null;
-        this.checkImplementations = new HashMap<>(); 
+        this.checkImplementations = new HashMap<>();
     }
-    
+
     /**
      * Checks if the archive has the name specified.
-     * 
+     *
      * @param inputFile
      * @param assignment
      */
-    private void checkArchiveName(File inputFile, Assignment assignment)
-    {
-        if ( null == assignment.getArchivename() )
-        {
+    private void checkArchiveName(File inputFile, Assignment assignment) {
+        if (null == assignment.getArchivename()) {
             return;
         }
-        
-        CheckResult checkResult = new CheckResult(); 
+
+        CheckResult checkResult = new CheckResult();
         checkResult.setPath(assignment.getArchivename());
         checkResult.setDescription("archive name matches");
         checkResult.setResultText(inputFile.getName());
-        
+
         // Pass if names match, otherwise fail.
-        if ( inputFile.getName().equals(assignment.getArchivename()) )
-        {
+        if (inputFile.getName().equals(assignment.getArchivename())) {
             checkResult.setOutcome(Outcome.PASS);
-        }
-        else
-        {
+        } else {
             checkResult.setOutcome(Outcome.FAIL);
-        } 
-        
+        }
+
         // Put it in the report
         this.report.post(checkResult);
     }
-    
+
     /**
      * Checks if the JAR/ZIP file has an entry at the path given by the content
-     * 
+     *
      * @param jarFile
      * @param content
      * @return the JarEntry
      */
-    private JarEntry getJarEntryForPath(JarFile jarFile, Content content)
-    {
-        CheckResult existenceResult = new CheckResult(); 
+    private JarEntry getJarEntryForPath(JarFile jarFile, Content content) {
+        CheckResult existenceResult = new CheckResult();
         existenceResult.setPath(content.getPath());
         existenceResult.setDescription("file exists");
         JarEntry jarEntry = jarFile.getJarEntry(content.getPath());
-        if ( null==jarEntry )
-        {
+        if (null == jarEntry) {
             existenceResult.setResultText("does not exist");
-            if ( content.isAdvisory() ) {
+            if (content.isAdvisory()) {
                 existenceResult.setOutcome(Outcome.ADVISORY);
             } else {
                 existenceResult.setOutcome(Outcome.FAIL);
             }
-        }
-        else
-        {
+        } else {
             existenceResult.setResultText("exists");
             existenceResult.setOutcome(Outcome.PASS);
         }
-        this.report.post(existenceResult);
-        
+        if ( !content.isCustom() ) {
+            this.report.post(existenceResult);
+        }
+            
         return jarEntry;
     }
-    
+
     /**
      * Runs all checks specified in the Assignment for the file.
-     * 
+     *
      * @param inputFile input JAR/ZIP file
      * @param assignment assignment tree
      */
-    public void runChecks(File inputFile, Assignment assignment) throws Exception
-    {
+    public void runChecks(File inputFile, Assignment assignment) throws Exception {
         // Clear the report if it exists
-        if ( null == this.report )
-        {
-            throw new Exception("Null report destination"); 
+        if (null == this.report) {
+            throw new Exception("Null report destination");
         }
-        
+
         // Clear the report
         report.clear();
-        
+
         // Set report title
-        report.setAssignmentName( assignment.getTitle() );
-        
+        report.setAssignmentName(assignment.getTitle());
+
         // Check if the file has the right name
         this.checkArchiveName(inputFile, assignment);
-        
+
         // JAR file
-        JarFile jarFile = new JarFile(inputFile); 
-        
+        JarFile jarFile = new JarFile(inputFile);
+
+        // Add in the pattern matched files for checking
+        // adapted from:
+        // http://stackoverflow.com/questions/9802785/find-all-resource-with-specific-extension-from-jar-programmatically
+        ArrayList<Content> autoContent = new ArrayList<>();
+        for (Content content : assignment.getContent()) {
+            autoContent.add(content);
+        }
+        for (Enumeration<JarEntry> em = jarFile.entries(); em.hasMoreElements();) {
+            String filePath = em.nextElement().toString();
+            for (Pattern pattern : assignment.getPattern()) {
+                if (filePath.endsWith(pattern.getMatch())) {
+                    Content content = new Content();
+                    content.setPath(filePath);
+                    content.setAdvisory(false);
+                    content.setCustom(true);
+                    for (Check check : pattern.getCheck()) {
+                        content.getCheck().add(check);
+                    }
+                    autoContent.add(content);
+                }
+            }
+
+        }
+
         // Loop over all files
-        for ( Content content : assignment.getContent() )
-        {
+        for (Content content : autoContent) {
+            
             // Check that file exists
             JarEntry jarEntry = this.getJarEntryForPath(jarFile, content);
-            
+
             // Run each check for the file
-            for ( Check check : content.getCheck() )
-            {   
-                CheckResult checkResult = new CheckResult(); 
+            for (Check check : content.getCheck()) {
+                CheckResult checkResult = new CheckResult();
                 checkResult.setPath(content.getPath());
-                
-                
+
                 try {
-                    CheckImplementation checkImplementation = this.getImplementationForCheck(check);  
-                    
+                    CheckImplementation checkImplementation = this.getImplementationForCheck(check);
+
                     checkResult.setDescription(checkImplementation.toString());
-                    
-                    if ( jarEntry == null )
-                    {
+
+                    if (jarEntry == null) {
                         checkResult.setResultText("(nonexistent file)");
                         checkResult.setOutcome(Outcome.SKIPPED);
+                    } else {
+                        checkImplementation.runCheck(jarFile.getInputStream(jarEntry), checkResult);
                     }
-                    else
-                    {
-                        checkImplementation.runCheck( jarFile.getInputStream( jarEntry ), checkResult );
-                    }
-                    
-                }
-                catch ( ClassNotFoundException e )
-                {
+
+                } catch (ClassNotFoundException e) {
 
                     checkResult.setDescription("(not found)");
                     checkResult.setResultText("(not found)");
                     checkResult.setOutcome(Outcome.CHECK_FAILURE);
-                }
-                catch ( IllegalAccessException | InstantiationException e  )
-                {
+                } catch (IllegalAccessException | InstantiationException e) {
                     checkResult.setDescription("(setup failure)");
                     checkResult.setResultText("(setup failure)");
-                    checkResult.setOutcome(Outcome.CHECK_FAILURE);   
-                }
-                catch ( Exception e )
-                {
+                    checkResult.setOutcome(Outcome.CHECK_FAILURE);
+                } catch (Exception e) {
                     e.printStackTrace();
                     checkResult.setDescription("(i/o exception occurred)");
                     checkResult.setOutcome(Outcome.CHECK_FAILURE);
-                }
-                finally
-                {
-                    if ( checkResult.getOutcome().causesFailure() && check.isAdvisory() ) {
+                } finally {
+                    if (checkResult.getOutcome().causesFailure() && check.isAdvisory()) {
                         checkResult.setOutcome(Outcome.ADVISORY);
                     }
-                    this.report.post(checkResult); 
+                    this.report.post(checkResult);
                 }
-                
+
             }
         }
-        
+
     }
-    
+
     /**
      * Return the implementation of a given check, and instantiate if needed.
+     *
      * @param check
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
-    private CheckImplementation getImplementationForCheck(Check check) throws ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
-        if ( !checkImplementations.containsKey(check) )
-        {
+    private CheckImplementation getImplementationForCheck(Check check) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        if (!checkImplementations.containsKey(check)) {
             CheckImplementation checkImplementation = (CheckImplementation) Class.forName("com.peadargrant.filecheck.core.checks." + check.getProcedure()).newInstance();
-            checkImplementation.applyParameters( check.getParameter() );
+            checkImplementation.applyParameters(check.getParameter());
             checkImplementations.put(check, checkImplementation);
         }
-        
+
         return checkImplementations.get(check);
     }
-    
+
 }
