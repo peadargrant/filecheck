2c80f3b89ba98b8b506025331d61217f50e3ce75
Peadar Grant <peadargrant@gmail.com>
2015-02-07 12:54:05 +0000

    refactored imports

--- filecheck-web/src/main/java/com/peadargrant/filecheck/web/controllers/CheckController.java
+++ filecheck-web/src/main/java/com/peadargrant/filecheck/web/controllers/CheckController.java
@@ -31,7 +31,6 @@ import java.io.FileOutputStream;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
