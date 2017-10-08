015b7afd935687a72166a44e720e678cff71b940
Peadar Grant <peadargrant@gmail.com>
2017-10-08 18:09:47 +0100

    pdf page counting

---
+++ filecheck-core/src/main/java/com/peadargrant/filecheck/core/checks/PDFPageCount.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 Peadar Grant
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.peadargrant.filecheck.core.checks;
+
+import com.peadargrant.filecheck.core.checker.CheckImplementation;
+import com.peadargrant.filecheck.core.checker.CheckResult;
+import com.peadargrant.filecheck.core.checker.Outcome;
+import java.io.InputStream;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+/**
+ *
+ * @author Peadar Grant
+ */
+public class PDFPageCount extends CheckImplementation {
+
+    private int minPages() {
+        return new Integer(this.stringParameters.get("minPages"));
+    }
+    
+    private int maxPages() {
+        return new Integer(this.stringParameters.get("maxPages"));
+    }
+    
+    @Override
+    public void runCheck(InputStream input, CheckResult cr) {
+
+        try {
+            PDDocument document = PDDocument.load(input);
+            
+            int nPages = document.getNumberOfPages();
+            
+            if ( ( nPages < minPages() ) || ( nPages > maxPages() )) {
+                cr.setOutcome(Outcome.FAIL);
+                cr.setResultText("page count " + nPages + " outside range " + minPages() + "-" + maxPages());
+                return;
+            }
+            
+            cr.setOutcome(Outcome.PASS);
+            cr.setResultText("page count " + nPages + " within range " + minPages() + "-" + maxPages());
+            
+        } catch (Exception e) {
+            cr.setResultText("(error occurred)");
+            cr.setOutcome(Outcome.CHECK_FAILURE);
+        }
+        
+        
+        
+    }
+    
+}
